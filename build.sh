185b08efb762cde732a81ae0cdf4ecadd38404d3
Wasin Thonkaew <haxpor@gmail.com>
2017-05-08 12:56:55 +0800

    Deployed via ghp-import

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-bundle exec jekyll build --future -d ./docs && echo "blog.wasin.io" > ./docs/CNAME && echo "done"
+#bundle exec jekyll build --future -d ./docs && echo "blog.wasin.io" > ./docs/CNAME && echo "done"
+bundle exec jekyll build --future -d ./docs && ghp-import -c 'blog.wasin.io' docs/ && echo 'done'
